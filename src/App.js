53abe195b1a0f9f6950d67be2db6804124a5142b
PhilipSamuelsson <philip.samuelsson@iths.se>
2023-03-24 20:21:47 +0100

    Navbar + Hero Section ##not finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+import NavBar from './Components/Navbar/NavBar'
+import HeroSection from './Components/HeroSection'
+
 function App() {
-    return <div className="App"></div>
+    return (
+        <div className="App">
+            <NavBar />
+            <HeroSection>Hejhej</HeroSection>
+        </div>
+    )
 }
 
 export default App
---
+++ src/Components/HeroSection.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import './HeroSection.css'
+
+export default function HeroSection() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container className="HeroImage" maxWidth="" disableGutters="false">
+                <Box sx={{ height: '60vh' }} />
+            </Container>
+        </React.Fragment>
+    )
+}
